176e727da978c62908e23784a32b2ca5966dc6c8
Yan Andreatta <yan.duca2@gmail.com>
2024-03-16 02:10:51 -0300

    Implementados as validações

--- app.js
+++ app.js
@@ -6,22 +6,21 @@ const valida = new ValidaFormulario();
 // seleciona os buttons
 const modalBtnRegister = document.querySelector(".register-btn");
 const modalBtnList = document.querySelector(".list-btn");
+const modalBtnNumbers = document.querySelector(".numbers-btn");
 
 // Seleciona as modais
 const modalOverlayConfirm = document.querySelector(".modal-overlay-confirm");
 const modalOverlayList = document.querySelector(".modal-overlay-list");
-
-// Incluir a funcionalidade Surprise e criar modal
-const modalOverlaySurprise = document.querySelector(".modal-overlay-surprise");
+const modalOverlayNumber = document.querySelector(".modal-overlay-numbers");
 
 // Seleciona os inputs
 const nameInput = document.querySelector(".name");
 const cpfInput = document.querySelector(".cpf");
-const cpfNumbers = document.querySelector(".numbers");
 
 // Seleciona o button de fechar modal
 const closeBtnConfirm = document.querySelector(".close-btn-confirm");
 const closeBtnList = document.querySelector(".close-btn-list");
+const closeBtnNumbers = document.querySelector(".close-btn-numbers");
 
 // Verifica se o botão "Registrar aposta" foi apertado
 modalBtnRegister.addEventListener("click", () => {
@@ -33,11 +32,10 @@ modalBtnList.addEventListener("click", () => {
     modalOverlayList.classList.add("open-modal");
 });
 
-// Verifica se o botão "Surpresinha" foi apertado
-// modalOverlaySurprise.addEventListener("click", e => {
-//     e.preventDefault();
-//     modalOverlaySurprise.classList.add("open-modal");
-// });
+// Verifica se o botão "Números da sorte" foi apertado
+modalBtnNumbers.addEventListener("click", (e) => {
+    modalOverlayNumber.classList.add("open-modal");
+});
 
 // Botão para fechar o modal
 closeBtnConfirm.addEventListener("click", (e) => {
@@ -46,8 +44,24 @@ closeBtnConfirm.addEventListener("click", (e) => {
 });
 
 closeBtnList.addEventListener("click", (e) => {
-    e.preventDefault(e);
+    e.preventDefault();
     modalOverlayList.classList.remove("open-modal");
 });
 
+closeBtnNumbers.addEventListener("click", (e) => {
+    e.preventDefault();
+    modalOverlayNumber.classList.remove("open-modal");
+});
 
+
+// Função para gerar os números da sorte
+function generateNumbers() {
+    const numerosContainer = document.getElementById("numbers");
+
+    for(let i = 1; i <= 50; i++) {
+        const numeroDiv = document.createElement("div");
+        numeroDiv.className.add("number");
+        numeroDiv.textContent = i;
+        numerosContainer.appendChild(numeroDiv);
+    }
+}
